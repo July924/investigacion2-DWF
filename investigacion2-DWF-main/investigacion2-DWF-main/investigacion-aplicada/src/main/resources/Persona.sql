bd89dd625ef6f0f71778074ca53ef209008b86b9
Alexa3912 <RM240112@alumno.udb.edu.sv>
2025-04-08 17:40:40 -0600

    Delete investigacion2-DWF-main/investigacion2-DWF-main directory

--- investigacion2-DWF-main/investigacion2-DWF-main/investigacion-aplicada/src/main/resources/Persona.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Persona (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100),
-    apellido VARCHAR(100)
-);
--- investigacion2-DWF-main/investigacion2-DWF-main/investigacion-aplicada/src/main/resources/insert.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Persona (nombre, apellido) VALUES ('Juan', 'Perez');
-INSERT INTO Persona (nombre, apellido) VALUES ('Maria', 'Gomez');
-INSERT INTO Persona (nombre, apellido) VALUES ('Carlos', 'Lopez');
