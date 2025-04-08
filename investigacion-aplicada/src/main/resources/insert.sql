3db419992d6157a1dd57ae3cefb4b3a6e9a5d453
Julissa <128512140+July924@users.noreply.github.com>
2025-04-08 15:53:05 -0600

    Primera Version

---
+++ investigacion-aplicada/src/main/resources/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO Persona (nombre, apellido) VALUES ('Juan', 'Perez');
+INSERT INTO Persona (nombre, apellido) VALUES ('Maria', 'Gomez');
+INSERT INTO Persona (nombre, apellido) VALUES ('Carlos', 'Lopez');
